5bc5bb151c38db36dd38899e93eb2acbff139104
Gourav-21 <rahul.dora21@gmail.com>
2024-06-24 04:58:30 +0530

    implimented delte

--- src/components/chat/chat-topbar.tsx
+++ src/components/chat/chat-topbar.tsx
@@ -1,52 +1,97 @@
 import React from 'react'
 import { Avatar, AvatarImage } from '../ui/avatar'
-import { UserData } from '@/app/data';
-import { Info, Phone, Video } from 'lucide-react';
+import { messagesProp } from '@/app/data';
+import { Info, Phone, Trash2, Video } from 'lucide-react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '../ui/button';
+import { deleteDoc, doc } from 'firebase/firestore/lite';
+import { db } from '@/lib/firebase';
 
 interface ChatTopbarProps {
-    selectedUser: UserData;
-    }
-    
-    export const TopbarIcons = [{ icon: Phone }, { icon: Video }, { icon: Info }];
+  selectedUser: messagesProp;
+}
 
+export default function ChatTopbar({ selectedUser }: ChatTopbarProps) {
+  
+  async function Delete(id: string) {
+    await deleteDoc(doc(db, "issues", id));
+    console.log("delete")
+  }
 
-export default function ChatTopbar({selectedUser}: ChatTopbarProps) {
   return (
     <div className="w-full h-20 flex p-4 justify-between items-center border-b">
-        <div className="flex items-center gap-2">
-          <Avatar className="flex justify-center items-center">
-            <AvatarImage
-              src={selectedUser.avatar}
-              alt={selectedUser.name}
-              width={6}
-              height={6}
-              className="w-10 h-10 "
-            />
-          </Avatar>
-          <div className="flex flex-col">
-            <span className="font-medium">{selectedUser.name}</span>
-            <span className="text-xs">Active 2 mins ago</span>
-          </div>
-        </div>
-
-        <div>
-          {TopbarIcons.map((icon, index) => (
-            <Link
-              key={index}
-              href="#"
-              className={cn(
-                buttonVariants({ variant: "ghost", size: "icon" }),
-                "h-9 w-9",
-                "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
-              )}
-            >
-              <icon.icon size={20} className="text-muted-foreground" />
-            </Link>
-          ))}
+      <div className="flex items-center gap-2">
+        <Avatar className="flex justify-center items-center">
+          <AvatarImage
+            src={selectedUser.avatar}
+            alt={selectedUser.name}
+            width={6}
+            height={6}
+            className="w-10 h-10 "
+          />
+        </Avatar>
+        <div className="flex flex-col">
+          <span className="font-medium">{selectedUser.name}</span>
+          <span className="text-xs">Active 2 mins ago</span>
         </div>
       </div>
+
+      <div>
+        <Link
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+          className={cn(
+            buttonVariants({ variant: "ghost", size: "icon" }),
+            "h-9 w-9",
+            "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+          )}
+        >
+          <Phone size={20} className="text-muted-foreground" />
+        </Link>
+        <Link
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+          className={cn(
+            buttonVariants({ variant: "ghost", size: "icon" }),
+            "h-9 w-9",
+            "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+          )}
+        >
+          <Video size={20} className="text-muted-foreground" />
+        </Link>
+        <Link
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            Delete(selectedUser.id);
+          }}
+          className={cn(
+            buttonVariants({ variant: "ghost", size: "icon" }),
+            "h-9 w-9",
+            "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+          )}
+        >
+          <Trash2 size={20} className="text-muted-foreground" />
+        </Link>
+        <Link
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+          className={cn(
+            buttonVariants({ variant: "ghost", size: "icon" }),
+            "h-9 w-9",
+            "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+          )}
+        >
+          <Info size={20} className="text-muted-foreground" />
+        </Link>
+      </div>
+    </div>
   )
 }

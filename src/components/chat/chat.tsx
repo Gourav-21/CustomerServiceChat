f790ab36d1c1f6ba9f8a03fd38ef7a56e2f647ae
Gourav-21 <rahul.dora21@gmail.com>
2024-06-24 13:54:44 +0530

    added send msg

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,9 +1,11 @@
-import { Message, UserData, messagesProp } from "@/app/data";
+import { Message, messagesProp } from "@/app/data";
 import ChatTopbar from "./chat-topbar";
 import { ChatList } from "./chat-list";
 import React, { useEffect } from "react";
-import { collection, getDocs, limit, query } from "firebase/firestore/lite";
+import { collection, getDocs, limit, query } from "firebase/firestore";
 import { db } from "@/lib/firebase";
+import { onSnapshot } from "firebase/firestore";
+import { addDoc } from "firebase/firestore";
 
 interface ChatProps {
   messages?: Message[];
@@ -12,9 +14,7 @@ interface ChatProps {
 }
 
 export function Chat({ messages, selectedUser, isMobile }: ChatProps) {
-  const [messagesState, setMessages] = React.useState<Message[]>(
-    messages ?? []
-  );
+  const [messagesState, setMessages] = React.useState<Message[]>([]);
 
   if (!selectedUser) {
     return <div className="flex flex-col justify-center items-center h-full text-2xl font-medium">
@@ -22,26 +22,39 @@ export function Chat({ messages, selectedUser, isMobile }: ChatProps) {
     </div>;
   }
 
+  
   useEffect(() => {
-    async function fetch() {
-      const q = query(collection(db, "issues", selectedUser.id, "messages"), limit(30));
-      const querySnapshot = await getDocs(q);
-      const messages = querySnapshot.docs.map((doc) => {
-        return { ...doc.data() }
-      });
-      setMessages(messages);
+    if (!selectedUser.id) {
+      return;
     }
-    fetch();
-  }, [messages]);
-
-
-
-  const sendMessage = (newMessage: Message) => {
-    setMessages([...messagesState, newMessage]);
-  };
-
+    const q = query(collection(db, "issues", selectedUser.id, "messages"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const newMessages = querySnapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+            setMessages((p)=>([...p,change.doc.data()]))
+        }
+      });
+      setMessages((p)=>([...p,newMessages])); // Update the state
+    });
+  
+    return () => {
+      unsubscribe();
+      setMessages([]);
+    };
+  }, [selectedUser]);
   
 
+
+
+  const sendMessage = async (newMessage: Message) => {
+    try {
+      const docRef = await addDoc(collection(db, "issues", selectedUser.id, "messages"), newMessage);
+      setMessages([...messagesState, newMessage]);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    // setMessages([...messagesState, newMessage]);
+  };
 
   return (
     <div className="flex flex-col justify-between w-full h-full">

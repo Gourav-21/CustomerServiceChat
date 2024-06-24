9b3f440d3613527f203f9a91a5f558b34e0910c8
Gourav-21 <rahul.dora21@gmail.com>
2024-06-25 01:03:28 +0530

    added env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCrm4HvLIh_Ildk12Y7Yvlx0f36yumCBRc",
-  authDomain: "chat-8639e.firebaseapp.com",
-  projectId: "chat-8639e",
-  storageBucket: "chat-8639e.appspot.com",
-  messagingSenderId: "577560535989",
-  appId: "1:577560535989:web:abca634f93c6812a92effc",
-  measurementId: "G-QZ5GDVLXKY"
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
 };
 
 // Initialize Firebase
